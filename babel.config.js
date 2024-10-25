07227e5dd96bd0a694bdd23a3c39e4d3a0ebdd88
H00manZ <H00man.fb.77@gmail.com>
2024-10-25 19:01:34 +0330

    add babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-// module.exports = {
-//     presets: [
-//       '@babel/preset-env',
-//       '@babel/preset-react', // For JSX
-//       '@babel/preset-typescript', // For TypeScript
-//     ],
-//   }
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react', // For JSX
+      '@babel/preset-typescript', // For TypeScript
+    ],
+  }
